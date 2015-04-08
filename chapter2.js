ab7acff25a14899773e0662f4f787cc34636c918
dierat <diedra.rater@gmail.com>
2015-04-07 21:24:15 -0700

    adds looping traingle exercise + solution

---
+++ chapter2.js
@@ -0,0 +1,28 @@
+/*
+
+Looping a triangle
+
+Write a loop that makes seven calls to console.log to output the following triangle:
+
+#
+##
+###
+####
+#####
+######
+#######
+
+*/
+
+var char = ''
+for (var i = 0; i < 7; i++) {
+	char += '#';
+	console.log(char);
+}
+
+
+/*
+
+
+
+*/
