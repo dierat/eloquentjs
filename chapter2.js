964332b5d512f583c8d6f31137ab27c04201861a
dierat <diedra.rater@gmail.com>
2015-04-09 14:04:17 -0700

    alternates between # and space depending on whether the iterator is even or odd in the interior loop

--- chapter2.js
+++ chapter2.js
@@ -67,8 +67,11 @@ var size=8;
 var str = '';
 for (var i=0; i<=size; i++) {
 	for (var j=0; j<=size; j++) {
-		str += '#';
+		if (j%2==0) {str += '#';}
+		else {str += ' '}
 	}
 	str += '\n';
 }
 console.log(str);
+
+
