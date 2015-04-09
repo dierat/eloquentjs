07cd2a6ee63aa5952fa5bd032eab0f29cec5e0f1
dierat <diedra.rater@gmail.com>
2015-04-09 14:09:44 -0700

    changes the for loop declarations to only print size number of rows and columns, not size+1

--- chapter2.js
+++ chapter2.js
@@ -65,8 +65,8 @@ When you have a program that generates this pattern, define a variable size = 8 
 
 var size=8;
 var str = '';
-for (var i=0; i<=size; i++) {
-	for (var j=0; j<=size; j++) {
+for (var i=0; i<size; i++) {
+	for (var j=0; j<size; j++) {
 		if (i%2==0) {
 			if (j%2==0) {str += ' ';}
 			else {str += '#'}
