7573e6749aecca331bd0e8c5a6cbdf4917b7a22e
dierat <diedra.rater@gmail.com>
2015-04-09 13:43:53 -0700

    adds Chess board question

--- chapter2.js
+++ chapter2.js
@@ -21,6 +21,7 @@ for (var i = 0; i < 7; i++) {
 }
 
 
+
 /*
 
 FizzBuzz
@@ -39,3 +40,26 @@ for (var i=1; i<=100; i++) {
   else if (i%5==0) {console.log('Buzz');}
   else {console.log(i)}
 }
+
+
+
+/*
+
+Chess board
+
+Write a program that creates a string that represents an 8×8 grid, using newline characters to separate lines. At each position of the grid there is either a space or a “#” character. The characters should form a chess board.
+
+Passing this string to console.log should show something like this:
+
+ # # # #
+# # # #
+ # # # #
+# # # #
+ # # # #
+# # # #
+ # # # #
+# # # #
+When you have a program that generates this pattern, define a variable size = 8 and change the program so that it works for any size, outputting a grid of the given width and height.
+
+*/
+
