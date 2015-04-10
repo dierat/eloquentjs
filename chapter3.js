eef6603806859081f7ac2db2e12f9ab6e863c900
dierat <diedra.rater@gmail.com>
2015-04-10 13:01:33 -0700

    adds solution for isEven

--- chapter3.js
+++ chapter3.js
@@ -5,6 +5,8 @@ function min(a,b) {
 	else {return b;}
 }
 
+
+
 /*
 
 We’ve seen that % (the remainder operator) can be used to test whether a number is even or odd by using % 2 to check whether it’s divisible by two. Here’s another way to define whether a positive whole number is even or odd:
@@ -21,3 +23,9 @@ Test it on 50 and 75. See how it behaves on -1. Why? Can you think of a way to f
 
 */
 
+function isEven(n) {
+	if (abs(n) <= 1) {return abs(n) == 0;}
+	else {
+		return isEven(abs(n) - 2);
+	}
+}
