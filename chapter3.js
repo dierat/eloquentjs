61f5c413b63f46d61aa82b86725f49f1c5768d21
dierat <diedra.rater@gmail.com>
2015-04-10 13:10:06 -0700

    adds Bean Counting question + Minimum and Recursion titles

--- chapter3.js
+++ chapter3.js
@@ -1,4 +1,10 @@
-// Write a function min that takes two arguments and returns their minimum.
+/*
+
+Minimum
+
+Write a function min that takes two arguments and returns their minimum.
+
+*/
 
 function min(a,b) {
 	if (a < b) {return a;}
@@ -8,6 +14,8 @@ function min(a,b) {
 
 
 /*
+
+Recursion
 
 We’ve seen that % (the remainder operator) can be used to test whether a number is even or odd by using % 2 to check whether it’s divisible by two. Here’s another way to define whether a positive whole number is even or odd:
 
@@ -30,3 +38,18 @@ function isEven(n) {
 	if (n <= 1) {return n == 0;}
 	else {return isEven(n - 2);}
 }
+
+
+
+/*
+
+Bean counting
+
+You can get the Nth character, or letter, from a string by writing "string".charAt(N), similar to how you get its length with "s".length. The returned value will be a string containing only one character (for example, "b"). The first character has position zero, which causes the last one to be found at position string.length - 1. In other words, a two-character string has length 2, and its characters have positions 0 and 1.
+
+Write a function countBs that takes a string as its only argument and returns a number that indicates how many uppercase “B” characters are in the string.
+
+Next, write a function called countChar that behaves like countBs, except it takes a second argument that indicates the character that is to be counted (rather than counting only uppercase “B” characters). Rewrite countBs to make use of this new function.
+
+*/
+
