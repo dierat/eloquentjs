251321740f1c905da810593200eedb7bdb495414
dierat <diedra.rater@gmail.com>
2015-04-22 13:52:58 -0700

    adds 'Reversing an array' problem

--- chapter4.js
+++ chapter4.js
@@ -41,3 +41,17 @@ console.log(sum(range(1, 10)));
 // → 55
 console.log(range(5, 2, -1));
 // → [5, 4, 3, 2]
+
+
+
+
+/*
+
+Reversing an array
+
+Arrays have a method reverse, which changes the array by inverting the order in which its elements appear. For this exercise, write two functions, reverseArray and reverseArrayInPlace. The first, reverseArray, takes an array as argument and produces a new array that has the same elements in the inverse order. The second, reverseArrayInPlace, does what the reverse method does: it modifies the array given as argument in order to reverse its elements. Neither may use the standard reverse method.
+
+Thinking back to the notes about side effects and pure functions in the previous chapter, which variant do you expect to be useful in more situations? Which one is more efficient?
+
+*/
+
