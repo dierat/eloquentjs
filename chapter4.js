d916d62a47589a08a8f496e824f902b56a926cb5
dierat <diedra.rater@gmail.com>
2015-05-03 15:01:58 -0700

    adds Deep Comparison problem

--- chapter4.js
+++ chapter4.js
@@ -167,3 +167,18 @@ console.log(prepend(10, prepend(20, null)));
 // → {value: 10, rest: {value: 20, rest: null}}
 console.log(nth(arrayToList([10, 20, 30]), 1));
 // → 20
+
+
+
+/*
+
+Deep comparison
+
+The == operator compares objects by identity. But sometimes, you would prefer to compare the values of their actual properties.
+
+Write a function, deepEqual, that takes two values and returns true only if they are the same value or are objects with the same properties whose values are also equal when compared with a recursive call to deepEqual.
+
+To find out whether to compare two things by identity (use the === operator for that) or by looking at their properties, you can use the typeof operator. If it produces "object" for both values, you should do a deep comparison. But you have to take one silly exception into account: by a historical accident, typeof null also produces "object".
+
+*/
+
