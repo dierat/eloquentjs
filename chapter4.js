05ff4eaa731335ce53e4b578cc39938c45f2296d
dierat <diedra.rater@gmail.com>
2015-05-03 15:02:38 -0700

    adds Deep Comparison test cases

--- chapter4.js
+++ chapter4.js
@@ -182,3 +182,12 @@ To find out whether to compare two things by identity (use the === operator for 
 
 */
 
+
+
+var obj = {here: {is: "an"}, object: 2};
+console.log(deepEqual(obj, obj));
+// → true
+console.log(deepEqual(obj, {here: 1, object: 2}));
+// → false
+console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
+// → true
