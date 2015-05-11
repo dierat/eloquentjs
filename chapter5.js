441ef827f126ae14ccd621f7c2dc48f951141339
dierat <diedra.rater@gmail.com>
2015-05-10 20:44:25 -0700

    adds inital historical life expectancy code

--- chapter5.js
+++ chapter5.js
@@ -36,18 +36,29 @@ ancestry.forEach(function(person) {
   byName[person.name] = person;
 });
 
-function average_mom_child_dif() {
+function average_mom_child_dif(obj) {
 	// first you need to find the ages of mothers when their children were born
 	// start with defining an array to hold the ages, then loop through all the people in ancestry and find out if that person's mother is present in the byName object. if she is, compute the mother's age at the time of the person's birth by subtracting the year of the person's birth from the year of her birth and add that to an array
 	// then run the mother/child age dif array through average and return the result
 	var ages = [];
-	for (var i=0; i<ancestry.length; i++) {
-		if (ancestry[i].mother in byName) {
-			ages.push(ancestry[i].born - byName[ancestry[i].mother].born);
+	for (var i=0; i<obj.length; i++) {
+		if (obj[i].mother in byName) {
+			ages.push(obj[i].born - byName[obj[i].mother].born);
 		}
 	}
 	return average(ages);
 }
+
+function average_mom_child_dif_2(obj) {
+	// first you need to find the ages of mothers when their children were born
+	// start with defining an array to hold the ages, then loop through all the people in ancestry and find out if that person's mother is present in the byName object. if she is, compute the mother's age at the time of the person's birth by subtracting the year of the person's birth from the year of her birth and add that to an array
+	// then run the mother/child age dif array through average and return the result
+	var hasMoms = obj.filter(function(person) {return person.mother in byName;});
+    var ages = hasMoms.map(function(person){return person.born - byName[person.mother].born});
+	return average(ages);
+}
+
+console.log(average_mom_child_dif(ancestry));
 
 // → 31.2
 
@@ -70,11 +81,17 @@ function average(array) {
   return array.reduce(plus) / array.length;
 }
 
-function historical_life_expenctancy() {
+function historical_life_expenctancy(array) {
 	// loop through the ancestry array and decide which century each person belongs to using the formula given passed to the groupBy function
 	// then run each century's array through the average function and return it
-
+	var centuries = groupBy(array, function(person) {return toString(Math.ceil(person.died / 100));})
+	for (century in centuries) {
+		return average(centuries[century]);
+	}
 }
+
+centuries = {'16': [{person1}{person2}{person3}], '17': [], }
+
 
 function groupBy(array, func) {
 	// computes the group for an element in the array and returns an object that maps group names to arrays of group members
@@ -82,7 +99,16 @@ function groupBy(array, func) {
 	// otherwise just add the person's age as a value to the array associated with that century
 	// then return the centuries object with all the ages neatly grouped by century
 	// keep in mind that the age property, and century formula will be given via the func argument
-
+	var groups = {};
+	for (var i=0; i<array.length; i++) {
+		var group = func(array[i]);
+		if (!group in groups) {
+			groups[group] = [array[i]];
+		} else {
+			groups[group].push(array[i]);
+		}
+	}
+	return groups;
 }
 
 // → 16: 43.5
