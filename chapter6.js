926c48c540ecfc3471dc63f0e7f9ea10af9d5b05
dierat <diedra.rater@gmail.com>
2015-05-15 16:09:55 -0700

    adds Vector solution

--- chapter6.js
+++ chapter6.js
@@ -11,6 +11,22 @@ Add a getter property length to the prototype that computes the length of the ve
 
 */
 
+function Vector(x,y) {
+    this.x = x;
+    this.y = y;
+}
+
+Vector.prototype.plus = function(vector2) {
+    return new Vector(this.x + vector2.x, this.y + vector2.y);
+}
+
+Vector.prototype.minus = function(vector2) {
+    return new Vector(this.x - vector2.x, this.y - vector2.y);
+}
+
+Object.defineProperty(Vector.prototype, "length", {
+  get: function() { return Math.sqrt( (this.x * this.x) + (this.y * this.y) ); }
+});
 
 
 console.log(new Vector(1, 2).plus(new Vector(2, 3)));
