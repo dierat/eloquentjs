88546b0a5180cd58c2db3c707327fdf2683e73a2
dierat <diedra.rater@gmail.com>
2015-05-15 18:00:05 -0700

    adds StretchCell solution

--- chapter6.js
+++ chapter6.js
@@ -46,6 +46,21 @@ Implement a cell type named StretchCell(inner, width, height) that conforms to t
 
 */
 
+function StretchCell(inner, width, height) {
+  this.inner = inner;
+  this.width = width;
+  this.height = height;
+}
+
+StretchCell.prototype.minWidth = function() {
+  return Math.max(this.width, this.inner.minWidth());
+};
+StretchCell.prototype.minHeight = function() {
+  return Math.max(this.height, this.inner.minHeight());
+};
+StretchCell.prototype.draw = function(width, height) {
+  return this.inner.draw(width, height);
+};
 
 
 var sc = new StretchCell(new TextCell("abc"), 1, 2);
